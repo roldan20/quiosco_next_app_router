deb2ee69da3a9075c0a085ed3ffa8584615c57c4
roldan20 <ralpuche@paisamex.mx>
2026-01-12 19:26:11 -0600

    cambios en index types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
-import { Order, OrderProducts, Product } from "@prisma/client";
+import { Order, OrderProducts, Product } from '@/app/generated/prisma'
 
 export type OrderItem = Pick<Product, 'id'|'name'|'price'> & {
     quantity:number
